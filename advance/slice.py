0ff4cd66bd0f2a27bdc2ada65e721f6f4121c421
Carlo <422247668@qq.com>
2017-09-18 15:06:02 +0800

    add: list slice and iteration example code

---
+++ advance/slice.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+L = ['Mike', 'Bob', 'Jim', 'Kimmy', 'Lily']
+
+print ('L[0:3] is', L[0:3])
+print ('L[:3] is', L[:3])
+print ('L[1:3] is', L[1:3])
+print ('L[1:2] is', L[1:2])
+print ('L[-2:] is', L[-2:])
+
+R = list(range(100))
+print ('R[:10] is', R[:10])
+print('R[-10:] is', R[-10:])
+print('R[10:20] is', R[10:20])
+print('R[:10:2] is', R[:10:2])
+print('R[::5] is', R[::5])
