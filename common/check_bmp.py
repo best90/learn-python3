b37f12d48d427d49ebd50da31a510b758df8012d
Carlo <422247668@qq.com>
2017-11-28 09:30:39 +0800

    add: base64 and struct usage

---
+++ common/check_bmp.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import struct
+#struct模块来解决bytes和其他二进制数据类型的转换
+bmp_header = b'\x42\x4d\x38\x8c\x0a\x00\x00\x00\x00\x00\x36\x00\x00\x00\x28\x00\x00\x00\x80\x02\x00\x00\x68\x01\x00\x00\x01\x00\x18\x00'
+print(struct.unpack('<ccIIIIIIHH', bmp_header))
