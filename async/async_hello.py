dcb2bcd3531444c74961f76406708547bb2c9a19
Carlo.J <422247668@qq.com>
2017-12-16 20:23:43 +0800

    add: async example

---
+++ async/async_hello.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+import threading
+import asyncio
+
+@asyncio.coroutine
+def hello():
+    print('Hello world! (%s)' % threading.currentThread())
+    # 异步调用asyncio.sleep(1)
+    yield from asyncio.sleep(1)
+    print('Hello again! (%s)' % threading.currentThread())
+
+loop = asyncio.get_event_loop()
+tasks = [hello(), hello()]
+loop.run_until_complete(asyncio.wait(tasks))
+loop.close()
