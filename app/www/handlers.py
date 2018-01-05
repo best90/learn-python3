73ae699af5e89c47deecc303642958afe4c288f3
J.Q <422247668@qq.com>
2018-01-05 15:36:04 +0800

    update: handlers

--- app/www/handlers.py
+++ app/www/handlers.py
@@ -9,10 +9,22 @@ from coroweb import get, post
 
 from models import User, Comment, Blog, next_id
 
-@get('/'):
-async def index(request):
-    users = await User.findAll()
+@get('/')
+def index(request):
+    summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    blogs = [
+        Blog(id='1', name='Test Blog', summary=summary, creeted_at=time.time()-200),
+        Blog(id='2', name='Something New', summary=summary, creeted_at=time.time()-3600),
+        Blog(id='3', name='Learn Swift', summary=summary, creeted_at=time.time()-7200)
+    ]
     return {
-        '__template__': 'test.html',
-        'users': users
+        '__template__': 'blogs.html',
+        'blogs': blogs
     }
+
+@get('/api/users')
+def api_get_users():
+    users = yield from User.findAll(orderBy='created_at desc')
+    for u in users:
+        u.passwd = '******'
+    return dict(users=users)
