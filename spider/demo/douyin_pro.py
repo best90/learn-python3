b816ebe09be05a1870074262ddb389b4b1dfd7ea
J.Q <422247668@qq.com>
2018-03-21 16:35:21 +0800

    [update] douyin video downloader and fixed bug

--- spider/demo/douyin_pro.py
+++ spider/demo/douyin_pro.py
@@ -14,7 +14,7 @@ class DouYin(object):
         """
         chrome_options = Options()
         chrome_options.add_argument('user-agent="Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36"')
-        self.driver = Browser(driver_name='chrome', executable_path='D:/chromedriver', options=chrome_options, headless=True)
+        self.driver = Browser(driver_name='chrome', executable_path='D:\Program Files\Python3\Scripts\chromedriver.exe', options=chrome_options, headless=True)
 
     def get_video_urls(self, user_id):
         video_names = []
@@ -41,3 +41,71 @@ class DouYin(object):
                 video_names.append(share_desc+'.mp4')
             video_urls.append(each['share_info']['share_url'])
         return video_names, video_urls, nickname
+
+    def get_download_url(self, video_url):
+        req = requests.get(url=video_url, verify=False)
+        bf = BeautifulSoup(req.text, 'lxml')
+        script = bf.find_all('script')[-1]
+        video_url_js = re.findall('var data = \[(.+)\];', str(script))[0]
+        video_html = json.loads(video_url_js)
+        download_url = video_html['video']['play_addr']['url_list'][0]
+        return download_url
+
+    def remove_watermark(self, video_url):
+        self.driver.visit('http://douyin.iiilab.com/')
+        self.driver.find_by_tag('input').fill(video_url)
+        self.driver.find_by_xpath('//button[@class="btn btn-default"]').click()
+        time.sleep(2)
+        html = self.driver.find_by_xpath('//div[@class="thumbnail"]/div/p')[0].html
+        bf = BeautifulSoup(html,'lxml')
+        return bf.find('a').get('href')
+
+
+    def video_downloader(self, video_url, video_name, watermark_flag=True):
+        size = 0
+        if watermark_flag == True:
+            video_url = self.remove_watermark(video_url)
+        else:
+            video_url = self.get_download_url(video_url)
+        with closing(requests.get(video_url, stream=True, verify=False)) as response:
+            chunk_size = 1024
+            content_size = int(response.headers['content-length'])
+            if response.status_code == 200:
+                sys.stdout.write(' [文件大小]:%0.2f MB\n' % (content_size/chunk_size/1024))
+
+                with open(video_name, 'wb') as file:
+                    for data in response.iter_content(chunk_size=chunk_size):
+                        file.write(data)
+                        size += len(data)
+                        file.flush()
+
+                        sys.stdout.write(' [下载进度]：%.2f%%' % float(size/content_size * 100) + '\r\n')
+                        sys.stdout.flush()
+
+    def run(self):
+        self.hello()
+        user_id = input('请输入ID（例如）:')
+        video_names, video_urls, nickname = self.get_video_urls(user_id)
+        if nickname not in os.listdir():
+            os.mkdir(nickname)
+        print('视频下载中：共有%d个作品！\n' % len(video_urls))
+        for num in range(len(video_urls)):
+            print(' 解析%d个视频链接[%s]中，请稍后！' % (num+1,video_urls[num]))
+            if '\\' in video_names[num]:
+                video_name = video_names[num].replace('\\', '')
+            if '/' in video_names[num]:
+                video_name = video_names[num].replace('/', '')
+            else:
+                video_name = video_names[num]
+            self.video_downloader(video_urls[num], os.path.join(nickname, video_name))
+            print('\n')
+
+
+    def hello(self):
+        print('*' * 100)
+        print('\t\t\t\t抖音App视频下载小助手')
+        print('*' * 100)
+
+if __name__ == '__main__':
+    douyin = DouYin()
+    douyin.run()
